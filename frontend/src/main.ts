7503d2b475bbed2acc5e8acdb607f88ac77401fa
huoshicang <74391377+huoshicang@users.noreply.github.com>
2024-06-16 15:56:28 +0000

    搭建初始页面

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,4 +2,16 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import ArcoVue from '@arco-design/web-vue';
+import '@arco-design/web-vue/dist/arco.css';
+
+import { router } from "./router/index"
+
+import { createPinia } from 'pinia'
+
+const pinia = createPinia()
+const app = createApp(App);
+app.use(ArcoVue);
+app.use(router);
+app.use(pinia)
+app.mount('#app');
